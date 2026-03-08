04ea92a152cb25c036c30b975834edb2edfae97c
jackiechalancon-svg <jackiechalancon2014@gmail.com>
2026-03-08 22:32:58 +0100

    Update cua_app.py

--- cua_app.py
+++ cua_app.py
@@ -1,1 +1,7 @@
+import streamlit as st
 
+st.title("Diagnostic CUA de la classe")
+
+st.write("Créatrice de l'application : Jacky Chalancon")
+
+st.write("Cette application permettra d'analyser le profil cognitif général d'une classe et l'environnement capacitant.")
